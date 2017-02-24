dbb24ff31b2088ce02cf87e8d099eb6382640d7f
kirk <ekirk0@gmail.com>
2017-02-24 10:56:47 -0800

    htdocs now gets environment variables on startup

--- htdocs/index.py
+++ htdocs/index.py
@@ -1,4 +1,21 @@
+import os
 from htdocs import webfaction
-from myapp import main
 
-application = webfaction.Middleware(main.app)
+
+def application(environ, start_response):
+    """
+    transport any environment variables from the apache
+    system into the flask wsgi application space
+    """
+    for key in environ:
+        if key.startswith('MYAPP_'):
+            os.environ[key] = environ[key]
+
+    # import the flask application
+    from myapp.main import app
+
+    # load some middleware to append the current appname to /
+    app.wsgi_app = webfaction.Middleware(app.wsgi_app)
+
+    return app(environ, start_response)
+
